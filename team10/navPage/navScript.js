b75aef4329104f3a9bebedef8e4b380e12e23595
nathanzhu144 <znathan@umich.edu>
2018-10-20 00:36:04 -0400

    Added comments to clarify nav pages

--- team10/navPage/navScript.js
+++ team10/navPage/navScript.js
@@ -1,3 +1,5 @@
+//This is the js file for the navPage
+
 $(document).ready(function(){
                   $('[data-toggle="offcanvas"]').click(function(){
                                                        $("#navigation").toggleClass("hidden-xs");
